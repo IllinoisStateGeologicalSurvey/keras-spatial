03ae27bd0b2fe91c44d7464304f0c38373b15198
Jeff Terstriep <jefft@illinois.edu>
2019-08-01 23:18:21 +0000

    Modify SDG to require patch height and width obsoleting the need for resolution.

--- src/keras_spatial/datagen.py
+++ src/keras_spatial/datagen.py
@@ -1,35 +1,51 @@
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numbers
 import geopandas as gpd
 import numpy as np
 import rasterio
+from rasterio.vrt import WarpedVRT
+from rasterio.warp import Resampling
 from rasterio.errors import RasterioIOError
 from pydap.client import open_url
 from pydap.net import HTTPError
 
+
 class SpatialDataGenerator(object):
 
-    def __init__(self, source=None, indexes=None, target_crs=None,
-            target_size=None, interpolation='nearest',
+    def __init__(self, width, height,
+            source=None, 
+            indexes=None, 
+            crs=None,
+            resampling=Resampling.nearest,
             shuffle=False):
         """
 
         Args:
+          width (int): patch width in pixels
+          height (int): patch height in pixels
           source (str): raster file path or OPeNDAP server
           indexes (int|[int]): raster file band (int) or bands ([int,...])
                                (default=None for all bands)
-          target_crs (str): proj4 definition defaults to dataframe.crs
+          crs (CRS): produces patches in different crs
+          resampling (str): interpolation method used with target_size
           shuffle (bool): shuffle batch of data
-          target_size ((int,int)): tuple with patch size
-          interpolation (str): interpolation method used with target_size
         """
 
-        self._mode = ''
-        self.src = None
-        self.source = source
+        self.width = width
+        self.height = height
+        if source: 
+            self.source = source
         self.indexes = indexes
-        self.target_size = target_size
-        self.interpolation = interpolation
+        self.crs=crs
+        self.resampling = resampling
         self.shuffle = shuffle
+
+    def _close(self):
+        if hasattr(self, 'src') and self.src:
+            self.src.close()
+            self.src = None
 
     @property
     def source(self):
@@ -43,63 +59,58 @@ class SpatialDataGenerator(object):
           source (str): local file path or URL using dap
         """
 
+        self._close()
         self._source = source
-        if not source:
-            return
-
-        # TODO if src is set close it?
-        if self.src:
-            self._mode = None
-            pass
 
         # try to use rasterio to open local file
         try:
-            self._mode = 'local'
             self.src = rasterio.open(source)
+            self._mode = 'local'
             return
         except RasterioIOError as e:
             pass
 
         # try to use pydap to open remote dataset
         try:
-            self._mode = 'dap'
             self.src = open_url(source)
+            self._mode = 'dap'
             return
         except HTTPError as e:
             pass
 
         raise OSError('source not found')
 
-    @property
-    def mode(self):
-        return self._mode
-
-    def get_batch(self, geometries):
+    def get_batch(self, src, geometries):
         """Get batch of patches from source raster
 
         Args:
+          src (rasterio): data source opened with rasterio
           geometries (GeoSeries): boundaries to extract from raster
 
         Returns:
           (numpy array)
+
+        This leverages rasterio's virtual warping to normalize data to
+        a consistent grid.
         """
-        
+
         batch = []
         for bounds in geometries.bounds.itertuples():
-            bot, left = self.src.index(bounds[1], bounds[2])
-            top, right = self.src.index(bounds[3], bounds[4])
-            window = rasterio.windows.Window(left, top, right-left, bot-top)
-            batch.append(self.src.read(indexes=self.indexes, window=window))
+            bot, left = src.index(bounds[1], bounds[2])
+            top, right = src.index(bounds[3], bounds[4])
+            window = rasterio.windows.Window(left, top, 
+                    right-left, bot-top)
+            batch.append(src.read(indexes=self.indexes, window=window))
+            print(batch[-1].shape)
 
         return np.stack(batch)
 
-    def flow_from_dataframe(self, dataframe, target_crs=None, batch_size=32):
+    def flow_from_dataframe(self, dataframe, batch_size=32):
         """extracts data from source based on dataframe extents
 
         Args:
           dataframe (geodataframe): dataframe with spatial extents
-          target_crs (str): proj4 definition defaults to dataframe.crs
-          batch_size (int): batch size to process
+          batch_size (int): batch size to process (default=32)
 
         Returns:
     
@@ -109,10 +120,27 @@ class SpatialDataGenerator(object):
         if self.shuffle:
             df = df.reindex(np.random.permutation(df.index))
 
-        if not df.crs == self.src.crs:
-             = df.to_crs(self.src.crs)
+        if self.crs:
+            df = df.to_crs(self.crs)
+
+        xres = df.bounds.apply(lambda row: row.maxx - row.minx, 
+                axis=1).mean() / self.width
+        yres = df.bounds.apply(lambda row: row.maxy - row.miny, 
+                axis=1).mean() / self.height
+
+        minx, miny, maxx, maxy = df.total_bounds
+        width = (maxx - minx) / xres
+        height = (maxy - miny) / yres
+        transform = rasterio.transform.from_origin(minx, maxy, xres, yres)
+
+        # use VRT to ensure correct projection and size
+        vrt = WarpedVRT(self.src, crs=df.crs, 
+                width=width, height=height,
+                transform=transform,
+                resampling=self.resampling)
 
         for i in range(0, len(df), batch_size):
-            yield self.get_batch(df.iloc[i:i+batch_size]['geometry'])
+            yield self.get_batch(vrt, df.iloc[i:i+batch_size]['geometry'])
 
+        vrt.close()
 
