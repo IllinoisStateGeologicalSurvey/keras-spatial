58a8d393eff5d134b4ce631af3c822c6b6a683bc
Jeff Terstriep <jefft@illinois.edu>
2019-11-18 18:37:36 +0000

    Create a example directly with code samples.

---
+++ examples/basic_example.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" 
+A simple, standalone example of using Keras Spatial to extract
+samples from a remote raster source.
+"""
+
+from keras_spatial import SpatialDataGenerator
+
+def example()
+    sdg = SpatialDataGenerator()
+    sdg.source = 'http://lidar.ncsa.illinois.edu:9000/test/mclean_roi.tif'
+
+    df = sdg.regular_grid(200, 200)
+
+    gen = sdg.flow_from_dataframe(df, 128, 128)
+    arr = next(gen)
+
+    print(arr.shape, arr.min(), arr.max())
+
+if __name__ == '__main__':
+    example()
